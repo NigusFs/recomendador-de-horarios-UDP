6de87070ac8d9552741dc70dcdf50668ae239554
NigusFs <nicolas.floress@mail.udp.cl>
2020-12-26 23:02:42 -0300

    extract_data sin comentarios adicionales listo para una demo

--- generador_de_horarios/extract_data.py
+++ generador_de_horarios/extract_data.py
@@ -1,34 +1,31 @@
-#crislo fue parte importante de este codigo. Aceptenlo en la practica. gracias :D Saludos !
+# Probado con la oferta academica 2019-1
 
-# Probado y testeado con la oferta academica 2019-1
-
-#se asume que el excel tiene la informacion correcta
 import numpy as np
 import pandas as pd
 
+#solo se consideran Catedra y Ayudantias, no Laboratorios
 
-def extract_data(arr_ramos_criticos,sheet_name='2019-1'): #get ramos criticos
-	count_cfg= arr_ramos_criticos.count("CFG")
-	excel = pd.read_excel('PROGRAMACION CURSOS 2017-2020.xlsx', sheet_name=sheet_name)
+def extract_data(arr_ramos_criticos,sheet_name='2019-1'): 
+
+	count_cfg= arr_ramos_criticos.count("CFG") #cuenta cuantos cfg se deben tomar 
+	excel = pd.read_excel('PROGRAMACION CURSOS 2017-2020.xlsx', sheet_name=sheet_name) #se obtiene los datos de la secciones de la oferta academica
 	excelArray = np.array(excel)
 
 	lista_secciones=[]
 
 	for i in range (0,len(excelArray)):
 		elem=excelArray[i]
-		#print(elem)
 		if isinstance(elem[5], str):
-			if elem[5][0] == "C":
-				aux_horario = [] # separar las catedras
-				#print(elem[4])
+			if elem[5][0] == "C": #se verifica que la informacion de la fila sea una Catedra
+				aux_horario = [] 
 				try:
-					if len(elem[7].split()) == 5:
-						aux = elem[7].split()[0]+" "+elem[7].split()[2] #if len(elem[7].split()) == 5 else elem[7].split()[0]+" "+elem[7].split()[1]
+					if len(elem[7].split()) == 5: #se procesa los datos de los horarios para usarlos posteriormente
+						aux = elem[7].split()[0]+" "+elem[7].split()[2] # se guarda el primer modulo de la Catedra ejemplo LU 08.30
 						aux_horario.append(aux)
-						aux = elem[7].split()[1]+" "+elem[7].split()[2]
+						aux = elem[7].split()[1]+" "+elem[7].split()[2] # se guarda el segundo modulo de la Catedra ejemplo MA 10.00
 						aux_horario.append(aux)
 
-					codigo = elem[4] #if isinstance(elem[4], str) == True else elem[1]
+					codigo = elem[4] 
 					nombre = elem[2]
 					seccion = elem[3]
 					profesor = elem[9]
@@ -36,25 +33,23 @@ def extract_data(arr_ramos_criticos,sheet_name='2019-1'): #get ramos criticos
 					pass 
 				finally:
 					continue
-			elif elem[5][0] == "A":
-				aux = elem[7].split()[0]+" "+elem[7].split()[1]
+			elif elem[5][0] == "A": #se verifica que la informacion de la fila sea una Ayudantia
+				aux = elem[7].split()[0]+" "+elem[7].split()[1]  # se guarda el primer modulo de la ayudantia ejemplo VI 17.25
 				aux_horario.append(aux)
 			else:
 				continue
 		else:
-			#print(elem[2],"->",type(elem[2]))
-			if nombre in arr_ramos_criticos :#and isinstance(excelArray[i+1][5], str) == False: # y si fuera un solo espacio ? # el si el utlima fila del excel terminar y fye
-				#print(excelArray[i+1][5])
+			if nombre in arr_ramos_criticos :
 				alfa = {'codigo':codigo,'nombre':nombre, 'seccion':seccion, "horario":aux_horario, "profesor":profesor}
 				aux_count = 0
-				for k in range(0,len(lista_secciones)):
-					if lista_secciones[k]["codigo"] == codigo:
+				for k in range(0,len(lista_secciones)): 
+					if lista_secciones[k]["codigo"] == codigo: # se verifica si ya existe esta seccion en la lista de secciones (se evitan datos repetidos)
 						aux_count+=1
 				if aux_count == 0:
 					lista_secciones.append(alfa)
 					
 
-	#pushea en un arreglo o dict
+	
 			
 	for i in range(0,count_cfg):
 		if count_cfg < 10:
@@ -69,4 +64,3 @@ def extract_data(arr_ramos_criticos,sheet_name='2019-1'): #get ramos criticos
 
 
 
-#print(extract_data(aux,int(count_cfg)))
